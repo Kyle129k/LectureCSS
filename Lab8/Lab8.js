21f61d12460dfd880b7e555492884b424478186f
Kyle129k <kjk48@njit.edu>
2024-11-13 23:57:43 -0500

    Lab 8

---
+++ Lab8/Lab8.js
@@ -0,0 +1,159 @@
+//one condition only, B is if its false//
+
+//Question 1//
+let firstname= "Kyle";
+console.log(Firstname);
+
+let age= 18
+console.log(age);
+
+//Question 2//
+
+//below is a string//
+let food= "Pineapple"
+console.log= (food);
+
+//below is a boolean with number//
+let house;
+
+let number= 100;
+
+if (number > 40)
+{
+    house = true;
+    console.log(house);
+}
+
+//This is undefined//
+
+let address;
+
+//Question 3//
+let RollNumber = 15;
+
+if (RollNumber > 10)
+{
+    RollNumber = true;
+    console.log(house);
+}
+
+    else{
+        RollNumber = false;
+        console.log(house);
+    }
+
+//Question 4//
+let isMember = 8;
+
+if (isMember < 10)
+{
+    isMember = true;
+    house = "Member discount applied";
+    console.log(house);
+}
+
+    else{
+        isMember = false;
+        console.log(house);
+    }
+
+//Question 5//
+
+let startValue = 50;
+
+if (startValue > 0)
+{
+    house = "Positive"
+    console.log(house);
+}
+
+else if (startValue < 0)
+{
+    house = "Negative"
+    console.log(house);
+}
+
+    else{
+        startValue = zero;
+        console.log(house);
+    }
+
+//Question 6//
+
+let a = 10;
+let b = 12;
+
+console.log(a == b);
+console.log(a === b);
+console.log(a != b);
+console.log(a !== b);
+console.log(a > b);
+console.log(a < b);
+console.log(a >= b);
+console.log(a <= b);
+
+//Question 7//
+let first_name = "Kyle";
+let last_name = "Klinsky";
+let my_age = 18;
+
+let message = 'Hello, ${first_name} ${last_name}, you are ${my_age} years old.';
+console.log(message);
+
+//Question 8//
+
+let numb = 50;
+
+if (num > 0 && numb <100) {
+    house = "Number is withing range"
+    console.log(house)
+}
+
+//Question 9//
+
+let hasCar = true;
+let hasLicense = true;
+let hasInsurance = true;
+if (hasCar && hasLicense && hasInsurance) {
+    house = "You can drive legally"
+    console.log(house)
+}
+else{
+    house = "Check your driving elegibility"
+}
+
+//Question 10//
+
+let num = 21;
+
+if (numb %2 ==0)
+{
+    house = "Number is even";
+    console.log(house)
+}
+
+else{
+    house = "Number is odd";
+    console.log(house);
+}
+
+//Question 11//
+
+let score = 85;
+
+if (score >=90)
+{
+    house = "Grade A";
+    console.log(house)
+}
+
+else if (80 <= score < 90)
+{
+    house = "Grade B";
+    console.log(house)
+}
+
+else{
+    house = "Grade C";
+    console.log(house);
+}
